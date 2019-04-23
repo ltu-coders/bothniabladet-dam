d5053eaa032ed8e1cc5be3fc71a5835e5c6d33a9
Ruslan Georgiev <ruslan.georgiev@hotmail.com>
2019-04-23 18:15:33 +0200

    Initial commit.

---
+++ src/main/java/se/ltucoders/bothniabladetdam/BothniabladetDamApplication.java
@@ -0,0 +1,13 @@
+package se.ltucoders.bothniabladetdam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BothniabladetDamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BothniabladetDamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/ltucoders/bothniabladetdam/BothniabladetDamApplicationTests.java
@@ -0,0 +1,16 @@
+package se.ltucoders.bothniabladetdam;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BothniabladetDamApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
