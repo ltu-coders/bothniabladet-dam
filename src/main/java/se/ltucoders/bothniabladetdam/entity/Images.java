5b0da32908b56f96dc43ea2e5783256eef9f4ac3
garnbutik <fredrik.harnevik@gmail.com>
2019-04-24 18:25:39 +0200

    Lagt till klassen Images.

---
+++ src/main/java/se/ltucoders/bothniabladetdam/entity/Images.java
@@ -0,0 +1,155 @@
+package se.ltucoders.bothniabladetdam.entity;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "Images")
+public class Images {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY.IDENTITY)
+    private int imageId;
+
+    private String fileName;
+
+    private String filePath;
+
+    private Users author;
+
+    private String description;
+
+    private String resolution;
+
+    private String fileSize;
+
+    private LocalDateTime dateTime;
+
+    private String make;
+
+    private String model;
+
+    private String location;
+
+    private String licenseType;
+
+    private int noOfAllowedUses;
+
+    private BigDecimal price;
+
+    public Images() {
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFilePath() {
+        return filePath;
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public Users getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Users author) {
+        this.author = author;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    public String getFileSize() {
+        return fileSize;
+    }
+
+    public void setFileSize(String fileSize) {
+        this.fileSize = fileSize;
+    }
+
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getLicenseType() {
+        return licenseType;
+    }
+
+    public void setLicenseType(String licenseType) {
+        this.licenseType = licenseType;
+    }
+
+    public int getNoOfAllowedUses() {
+        return noOfAllowedUses;
+    }
+
+    public void setNoOfAllowedUses(int noOfAllowedUses) {
+        this.noOfAllowedUses = noOfAllowedUses;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
