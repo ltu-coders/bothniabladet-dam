cc363b2d587e26eceb827c25e286b08da3f9ec5f
garnbutik <fredrik.harnevik@gmail.com>
2019-05-01 14:41:45 +0200

    Fixat mer med Image och ImageUse.

--- src/main/java/se/ltucoders/bothniabladetdam/db/ImageRepository.java
+++ src/main/java/se/ltucoders/bothniabladetdam/db/ImageRepository.java
@@ -2,6 +2,7 @@ package se.ltucoders.bothniabladetdam.db;
 
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +12,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -77,16 +81,55 @@ public class ImageRepository {
         }
         return theImage;
     }
-    public List<Image> advancedSearch(SearchCriteria searchCriteria) {
+    @Transactional
+    public List<Image> search(SearchCriteria searchCriteria) {
 
         try (Session session = entityManager.unwrap(Session.class)){
-            CriteriaBuilder criteriaBuilder = session.getCriteriaBuilder();
-            CriteriaQuery<Image> criteriaQuery = criteriaBuilder.createQuery(Image.class);
+            String sql = "select distinct it.imageId from ImagesTags it " +
+                    "inner join Tags t " +
+                    "where t.tagId = it.tagId AND (t.tagName IN :tags)";
+
+            //Fetches the id's of the images mapped to specific tags
+            NativeQuery query = session.createNativeQuery(sql);
+            query.setParameterList("tags", searchCriteria.getTags());
+
+            List<Integer> imagesId = query.getResultList();
+
+            CriteriaBuilder cBuilder = session.getCriteriaBuilder();
+            CriteriaQuery<Image> criteriaQuery = cBuilder.createQuery(Image.class);
             Root<Image> imageRoot = criteriaQuery.from(Image.class);
 
-            criteriaQuery.select(imageRoot);
+
+            List<Predicate> tagPredicate = new ArrayList<>();   //Holds the list of id's to be added to the OR condition
+            List<Predicate> predicateList = new ArrayList<>();  //in the where clause
+
+            //If there are > 0 tags add them as predicates
+            if (searchCriteria.getTags().length > 0) {
+                for (int loop = 0; loop < imagesId.size(); loop++) {
+                    tagPredicate.add(cBuilder.equal(imageRoot.get("imageId"), imagesId.get(loop)));
+                }
+            }
+            //if author is a search criteria, add that as a predicate
+            if (searchCriteria.getAuthor()>0){
+                predicateList.add(cBuilder.equal(imageRoot.get("author"), searchCriteria.getAuthor()));
+            }
+            if (searchCriteria.getFromDate() != null) {
+                predicateList.add(cBuilder.greaterThanOrEqualTo(imageRoot.get("dateTime"), searchCriteria.getFromDate()));
+            }
+            if (searchCriteria.getToDate() != null) {
+                predicateList.add(cBuilder.lessThanOrEqualTo(imageRoot.get("dateTime"), searchCriteria.getToDate()));
+            }
+
+            //Add the list of imageId predicates as an OR condition
+            Predicate keywords = cBuilder.or(tagPredicate.toArray(new Predicate[0]));
+
+            //Adds the other criterias as AND conditions
+            Predicate finalPredicate = cBuilder.and(predicateList.toArray(new Predicate[0]));
+            criteriaQuery.where(finalPredicate, keywords).distinct(true);
             org.hibernate.query.Query<Image> imageQuery = session.createQuery(criteriaQuery);
-            List<Image> images = imageQuery.getResultList();
+
+            return imageQuery.getResultList();
+
         } catch (HibernateException ex) {
             ex.printStackTrace();
         }
