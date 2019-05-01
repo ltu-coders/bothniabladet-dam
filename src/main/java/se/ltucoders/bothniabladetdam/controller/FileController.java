6922324c61fad8e54c7824e4a4b9767fb7471e3b
Flingor <mihail.cobzev@yahoo.com>
2019-05-01 13:09:12 +0200

    Added PUT method for changing image information

--- src/main/java/se/ltucoders/bothniabladetdam/controller/FileController.java
+++ src/main/java/se/ltucoders/bothniabladetdam/controller/FileController.java
@@ -2,7 +2,6 @@ package se.ltucoders.bothniabladetdam.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import se.ltucoders.bothniabladetdam.exception.FileStorageException;
 import se.ltucoders.bothniabladetdam.property.FileContentTypeProperties;
 
 @Controller
